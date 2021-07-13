84ace575cc4dfeedb9b5d81e4da33e6cc6812790
gksrlf <59948163+gksrlf@users.noreply.github.com>
2021-07-13 17:01:56 +0900

    CH03_03 두 개의 화면을 가진 웹앱

---
+++ FastCampus/javascript_typescript/CH02_04.ts
@@ -0,0 +1,7 @@
+type Centimeter = Number;
+type RaninbowColor = 'red' | 'orange' | 'yellow' | 'green' | 'blue' | 'pupple';
+let age = 10;
+let weight:number = 80;
+let height:Centimeter = 176;
+let color:RaninbowColor = 'orange';
+
