f9ac4e2a7aa8a2bc3028be6364975eddda97ce75
gksrlf <59948163+gksrlf@users.noreply.github.com>
2021-08-06 15:13:18 +0900

    Delete FastCampus directory

--- FastCampus/javascript_typescript/CH02_04.ts
+++
@@ -1,7 +0,0 @@
-type Centimeter = Number;
-type RaninbowColor = 'red' | 'orange' | 'yellow' | 'green' | 'blue' | 'pupple';
-let age = 10;
-let weight:number = 80;
-let height:Centimeter = 176;
-let color:RaninbowColor = 'orange';
-
